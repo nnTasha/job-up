4a61f1075a4b87c927fe395ec69b1fa723d40402
Natalia Sargarovschi <natalia.sargarovschi@amdaris.com>
2023-11-01 16:16:12 +0200

    SVG loader plugin configuration

--- app/src/containers/LandingPage.tsx
+++ app/src/containers/LandingPage.tsx
@@ -1,15 +1,14 @@
 import { Box, Container } from '@mui/material';
-// import DeskImage from '../assets/desk.svg?component';
+import DeskImage from '../assets/desk.svg?react';
 
 const LandingPage = () => {
   return (
     <>
       <Container sx={{ display: 'flex' }}>
-        <Box sx={{ border: '1px solid red', flexGrow: 1 }}>
-          {'an image '}
-          {/* <DeskImage /> */}
+        <Box sx={{ border: '1px solid red', flexGrow: 1 }}>{'some text '}</Box>
+        <Box sx={{ border: '1px solid green', flexGrow: 1 }}>
+          <DeskImage />
         </Box>
-        <Box sx={{ border: '1px solid green', flexGrow: 1 }}>{'an image '}</Box>
       </Container>
     </>
   );
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // resolve: {
-  //   alias: [{ find: '@', replacement: path.resolve(__dirname, 'app/src') }],
-  // },
-  publicDir: './public',
+  plugins: [react(), svgr()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'app/src/main.tsx'),
-    },
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  publicDir: './public',
 });
