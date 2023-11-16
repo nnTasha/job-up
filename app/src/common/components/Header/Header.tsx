4794a8cc35cd5e7b038bd7e9b0c0cd2b581e8dce
Natalia Sargarovschi <natalia.sargarovschi@amdaris.com>
2023-11-16 20:44:44 +0200

    Add SignIn, SignUp navigation path

--- app/src/common/components/Header/Header.tsx
+++ app/src/common/components/Header/Header.tsx
@@ -1,8 +1,20 @@
 import { Box, Button, Grid } from '@mui/material';
+import { useNavigate } from 'react-router';
 import Label from '../Label/Label';
+import { formatPath } from './utils';
 import { headerStyles } from './styles';
+import { useState } from 'react';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const [pathToNavigate, setPath] = useState('');
+  const handleNavigationClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const newPath = formatPath(e.currentTarget.innerText);
+    setPath(newPath);
+    navigate(`/${newPath}`);
+  };
+
   return (
     <Grid
       container
@@ -15,8 +27,16 @@ const Header = () => {
       </Grid>
       <Grid item>
         <Box display="flex" alignItems="center">
-          <Button sx={headerStyles.button}>Sign up</Button>
-          <Button variant="contained">Sign In</Button>
+          {!pathToNavigate && (
+            <>
+              <Button sx={headerStyles.button} onClick={handleNavigationClick}>
+                Sign up
+              </Button>
+              <Button variant="contained" onClick={handleNavigationClick}>
+                Sign In
+              </Button>
+            </>
+          )}
         </Box>
       </Grid>
     </Grid>
